4cd9f7c4c93732be3bb32b51bb3cbd9e01441a3d
PaayalKapoor <paayalkapoor2018@gmail.com>
2024-12-25 17:32:39 +0530

    Profile page added

---
+++ week4/isa_task_app/lib/notification_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class NotificationScreen extends StatelessWidget {
+  const NotificationScreen({super.key});
+  @override
+  Widget build(context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Text('Notification Screen'),
+    );
+  }
+}
